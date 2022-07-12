04b262c9e9991a1b0eff34283be727eb3060c49b
axdborges <axdborges@outlook.com>
2022-07-12 10:48:32 -0300

    feat: requisição login completa

--- src/models/login.model.js
+++ src/models/login.model.js
@@ -1,3 +1,5 @@
+import Requisicoes from "./Requisicoes.models.js";
+
 export default class FormLogin {
     static registerForm = document.getElementById("login_form");
     static buttonForm = document.querySelector(".button_form");
@@ -13,36 +15,7 @@ export default class FormLogin {
             data[item.name] = item.value;
             }
         }
-        await FormLogin.loginUser(data)
-    }
-
-    static async loginUser(data) {
-        const response = await fetch(
-            "https://habits-kenzie.herokuapp.com/api/userLogin",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(data), 
-            }
-        )
-        .then((res) => res.json())
-        .then((res) => {
-            if(res.token){
-                localStorage.setItem("userId", res.userId)
-                localStorage.setItem("token", res.token)
-                console.log(localStorage.getItem("token"))
-                // window.location.replace("/src/pages/principal.html")
-            } else {
-                alert("login invalido")
-            }
-            
-            return res
-        })
-        .catch((error) => console.log(error));
-        
-        return response;
+        await Requisicoes.userLogin(data)
     }
 
 }
