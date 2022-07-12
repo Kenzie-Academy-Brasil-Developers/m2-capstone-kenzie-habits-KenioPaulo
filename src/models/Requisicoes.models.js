64c0e10a8fafe73460f0c43671331a0a6fe917b5
Lucas Barcelos Meira <lucasxmb800@gmail.com>
2022-07-12 10:20:47 -0300

    feat: finalizado requisicao

--- src/models/Requisicoes.models.js
+++ src/models/Requisicoes.models.js
@@ -43,4 +43,69 @@ export default class Requisicoes {
         .then(response => response.json())
         .catch(err => console.log(err))
     }
+
+    static async realAll(){
+        return await fetch(`${this.base_url}/habits`,{
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("@kenzie:token")}`
+            }
+        })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+    }
+
+    static async readByCategory(category){
+            return await fetch(`${this.base_url}/habits/category/${category}`,{
+
+                method:"GET",
+                headers:{
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("@kenzie:token")}`
+                }
+            })
+            .then(response => response.json())
+            .catch(err => console.log(err))
+    }
+
+    static async updateHabit(habitId, data){
+        return await fetch(`${this.base_url}/habits/${habitId}`,{
+           method: "PATCH",
+           headers:{
+            "Content-Type" : "application/json",
+            Authorization:`Bearer ${localStorage.getItem("@kenzie:token")}`
+           }, 
+           body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+    } 
+
+    static async completeHabit(habitId){
+        return await fetch(`${this.base_url}/habits/complete/${habitId}`,{
+
+            method:"PATCH",
+            headers:{
+                "Content-Type" : "application/json",
+                Authorization:`Bearer ${localStorage.getItem("@kenzie:token")}`
+            },
+            
+        })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+
+    }
+
+    static async deleteHabit(habitId) {
+        return await fetch(`${this.base_url}/habits/${habitId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("@kenzie:token")}`
+            }
+        })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+    }
 }
