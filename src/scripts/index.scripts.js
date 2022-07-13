313464fd95cd769dcdf0f018eaf4ef90f72b2e2d
Victor Cruz <motoggb979@gmail.com>
2022-07-13 20:49:48 -0300

    feat: enviar para page login

--- src/scripts/index.scripts.js
+++ src/scripts/index.scripts.js
@@ -4,13 +4,13 @@ import Modais from "../controller/Modais.controller.js";
 import {botaoEditarPerfil, botaoLogout, botaoCriarHabito, botaoCheck, botaoEditarTarefa } from "../controller/Botoes.controller.js";
 import {btnConcluidos} from "../controller/Filtro.controller.js"
 
-const userName     = document.querySelector(".user_name ")
-const userImg      = document.querySelector(".img_menu")
-const userHeader   = document.querySelector(".img_header")
-const user         = JSON.parse(localStorage.getItem("@kenzie:user"))
-userImg.src        = user.usr_image
-userName.innerText = user.usr_name
-userHeader.src     = user.usr_image
+// const userName     = document.querySelector(".user_name ")
+// const userImg      = document.querySelector(".img_menu")
+// const userHeader   = document.querySelector(".img_header")
+// const user         = JSON.parse(localStorage.getItem("@kenzie:user"))
+// userImg.src        = user.usr_image
+// userName.innerText = user.usr_name
+// userHeader.src     = user.usr_image
 
 
 await Requisicoes.readAll()
@@ -21,3 +21,16 @@ botaoCriarHabito()
 botaoCheck()
 botaoEditarTarefa()
 btnConcluidos()
+
+
+if (localStorage.getItem("@kenzie:token") == null){
+  window.location.replace("src/views/login.views.html")
+} else {
+  const userName     = document.querySelector(".user_name ")
+  const userImg      = document.querySelector(".img_menu")
+  const userHeader   = document.querySelector(".img_header")
+  const user         = JSON.parse(localStorage.getItem("@kenzie:user"))
+  userImg.src        = user.usr_image
+  userName.innerText = user.usr_name
+  userHeader.src     = user.usr_image
+}
