de72c8586e44153a354eb50d21337a43933c56e7
Lucas Barcelos Meira <lucasxmb800@gmail.com>
2022-07-13 13:57:38 -0300

    feat:funcionalidade botoes

--- src/scripts/index.scripts.js
+++ src/scripts/index.scripts.js
@@ -2,7 +2,9 @@ import Requisicoes from "../models/Requisicoes.models.js";
 import Tabela from "../models/tabela.model.js";
 import Modais from "../controller/Modais.controller.js";
 
-const userName     = document.querySelector(".user_name")
+
+
+const userName     = document.querySelector(".user_name ")
 const userImg      = document.querySelector(".img_menu")
 const userHeader   = document.querySelector(".img_header")
 const user         = JSON.parse(localStorage.getItem("@kenzie:user"))
@@ -16,7 +18,26 @@ window.addEventListener("onload" , () => {
 })
 
 
+
+
 Requisicoes.readAll()
 
-Modais.criarHabito()
+// Modais.criarHabito()
 // Modais.editarHabito()
+
+// Modais.botaofechar.addEventListener("click",()=>{
+//     Modais.corpoPrincipal.classList.add("remove")
+// })
+
+
+/////////////////////// funcionado/////////////////////
+
+const botaoCriar = document.querySelector(".bttn_Criar")
+botaoCriar.addEventListener('click', () => {
+    Modais.criarHabito()
+})
+
+
+Tabela.botaoEditar.addEventListener("click", () => {
+    Modais.editarHabito()
+})
