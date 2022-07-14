d7fdf150acad71d93a3400cb69afedcfb3436aa4
axdborges <axdborges@outlook.com>
2022-07-14 10:29:44 -0300

    feat: efeito visual nas tarefas checadas

--- src/scripts/index.scripts.js
+++ src/scripts/index.scripts.js
@@ -4,25 +4,12 @@ import Modais from "../controller/Modais.controller.js";
 import {botaoEditarPerfil, botaoLogout, botaoCriarHabito, botaoCheck, botaoEditarTarefa } from "../controller/Botoes.controller.js";
 import {btnConcluidos} from "../controller/Filtro.controller.js"
 
-// const userName     = document.querySelector(".user_name ")
-// const userImg      = document.querySelector(".img_menu")
-// const userHeader   = document.querySelector(".img_header")
-// const user         = JSON.parse(localStorage.getItem("@kenzie:user"))
-// userImg.src        = user.usr_image
-// userName.innerText = user.usr_name
-// userHeader.src     = user.usr_image
-
-
 await Requisicoes.readAll()
-
-// ate aqui
  
 const login = {
     "email": "grupo3Nicole@mail.com",
   "password": "343e627759cd17520661ad15047a3c8a"
 }
-
-
 
 botaoLogout()
 botaoCriarHabito()
@@ -43,4 +30,6 @@ if (localStorage.getItem("@kenzie:token") == null){
   userName.innerText = user.usr_name
   userHeader.src     = user.usr_image
 }
+
+
 
